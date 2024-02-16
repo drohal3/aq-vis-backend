5a4a961177b782e3ecefe1723c2f6158bf777273
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-02-16 17:37:54 +0200

    measurements API

--- app/src/api/measurements.py
+++ app/src/api/measurements.py
@@ -12,12 +12,14 @@ TABLE = "cpc_measurements_test"
 router = APIRouter()
 config = DotEnvConfig()
 @router.get("")
-def read_items(date_time_from, date_time_to, device_id, current_user: User = Depends(get_current_active_user)):
+def read_items(date_time_from: str, date_time_to: str, device_id: int, current_user: User = Depends(get_current_active_user)):
     dynamodb = boto3.resource('dynamodb',
                               aws_access_key_id=config.get_config(config.ENV_AWS_ACCESS_KEY_ID),
                               aws_secret_access_key=config.get_config(config.ENV_AWS_SECRET_ACCESS_KEY),
                               region_name=config.get_config(config.ENV_AWS_REGION_NAME),
                               )
+
+    print(f">>>> >>>> date_time_from: {date_time_from}")
 
     table = dynamodb.Table(TABLE)
     # DATE_FROM = '2023-11-23 12:30:00'
