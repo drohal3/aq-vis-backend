974ea350a428e916c8074ed9a9c2e6f01347a41f
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-02-20 16:01:25 +0200

    create user API moved to admin

--- app/src/api/admin/admin.py
+++ app/src/api/admin/admin.py
@@ -1,11 +1,17 @@
 from fastapi import APIRouter, Depends, HTTPException
 from src.api.admin.organisations import router as organisations_router
+from src.api.admin.users import router as users_router
 
 routers = {
     "organisations": {
         "router": organisations_router,
         "prefix": "/organisations",
         "tags": ["admin organisations"]
+    },
+    "users": {
+        "router": users_router,
+        "prefix": "/users",
+        "tags": ["admin users"]
     }
 }
 
