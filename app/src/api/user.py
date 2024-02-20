4833ac408cb59de4d8896882bfb41e4ad48efc71
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-02-20 16:01:13 +0200

    create user API moved to admin

--- app/src/api/user.py
+++ app/src/api/user.py
@@ -11,43 +11,6 @@ import logging
 
 router = APIRouter()
 
-@router.post("/", response_model=User)
-async def create_user(form_data: NewUser):
-    database = router.database
-    password = form_data.password
-    hashed_password = get_password_hash(password)
-
-    new_user_data = form_data.model_dump()
-    new_user_data.pop("password")
-    new_user_data["hashed_password"] = hashed_password
-    user_id = database.users.insert_one(new_user_data).inserted_id
-    logging.debug(f"create_user() - user_id: {user_id}")
-    user = database.users.find_one({"_id": user_id})
-
-    logging.info(f"create_user() - created user: {user}")
-
-    user["id"] = str(user["_id"])
-
-    return user
-
-@router.put("/", response_model=User)
-async def update_user(form_data: User, current_user: User = Depends(get_current_active_user)):
-    database = router.database
-    if current_user.id != form_data.id:
-        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Unauthorized")
-
-    data = form_data.model_dump()
-    user_id = data["id"]
-
-    del data["id"]
-
-    database.users.update_one({"_id": ObjectId(user_id)}, {"$set": data})
-    updated_user = database.users.find_one({"_id": ObjectId(user_id)})
-    updated_user["id"] = str(updated_user["_id"])
-
-    return updated_user
-
-
 @router.get("/me", response_model=User)
 async def read_user_me(current_user: User = Depends(get_current_active_user)):
     return current_user
