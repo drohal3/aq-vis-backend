77db107c972d5a96198706636f61848e704deb9e
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-02-22 10:33:36 +0200

    parameters with unit added to device

--- app/src/models/device.py
+++ app/src/models/device.py
@@ -1,10 +1,15 @@
 from pydantic import BaseModel
 
+class Parameter(BaseModel):
+    name: str
+    unit: str  # unit_id
+    description: str | None = None
+
 class NewDevice(BaseModel):
     name: str
     code: str
     organisation: str
-    parameters: list[str] = []
+    parameters: list[Parameter] = []
 
 class Device(NewDevice):
     id: str
