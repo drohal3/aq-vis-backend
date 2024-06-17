073f5ffd68d949fabbc2c5ce7115bf372d51e3d0
Dominik Rohal <rohal.dominik.123@gmail.com>
2024-06-17 10:30:28 +0300

    linting

--- app/src/models/device.py
+++ app/src/models/device.py
@@ -6,14 +6,28 @@ class Parameter(BaseModel):
     name: str
     code: str
     unit: str  # unit_id
-    description: str | None = Field(default=None, title="Description of the parameter")
+    description: str | None = Field(
+        default=None, title="Description of the parameter"
+    )
 
 
 class DeviceBase(BaseModel):
-    name: str = Field(..., min_length=3, description="The length must be at least 3 characters")
-    code: str = Field(..., title="The code is used to identify the device in external systems (i.e. IoT data)")
-    organisation: str = Field(..., title="Reference to the Organisation the device belongs to")
-    parameters: list[Parameter] = Field(default=[], title="List of parameters the device is able to measure")
+    name: str = Field(
+        ...,
+        min_length=3,
+        description="The length must be at least 3 characters",
+    )
+    code: str = Field(
+        ...,
+        title="The code is used to identify the device"
+        " in external systems (i.e. IoT data)",
+    )
+    organisation: str = Field(
+        ..., title="Reference to the Organisation the device belongs to"
+    )
+    parameters: list[Parameter] = Field(
+        default=[], title="List of parameters the device is able to measure"
+    )
 
 
 class DeviceIn(DeviceBase):
